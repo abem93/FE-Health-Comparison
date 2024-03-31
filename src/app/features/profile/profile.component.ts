f070e7e1834d4cc75fee49c54376ac5f4483e992
abem93 <aimejia4@gmail.com>
2024-03-30 23:44:38 -0500

    added edit option to profile

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../core/services/user.service';
 import { User } from '../../shared/model/user';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent implements OnInit {
-  constructor(private userService: UserService) { }
+  editMode: boolean = false;
   user: User | null = null
+
+  constructor(private userService: UserService) { }
+
 
   ngOnInit(): void {
     this.userService.currentUserBehaviorSubject.subscribe((user) => {
@@ -21,5 +25,9 @@ export class ProfileComponent implements OnInit {
   currentUser() {
     console.log( this.userService.currentUserBehaviorSubject)
 
+  }
+
+  isEditing() {
+    this.editMode = !this.editMode
   }
 }
