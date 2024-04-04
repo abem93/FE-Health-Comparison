8c3053fc4c0f96261a0921cfd6d1556db24fc7c9
abem93 <aimejia4@gmail.com>
2024-04-04 00:05:22 -0500

    mapbox integration added

--- src/app/features/procedure/procedure.component.ts
+++ src/app/features/procedure/procedure.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { SpinnerComponent } from '../../shared/components/spinner/spinner.component';
+import { MapComponent } from '../../shared/components/map/map.component';
+
 
 @Component({
   selector: 'app-procedure',
   standalone: true,
-  imports: [SpinnerComponent],
+  imports: [SpinnerComponent, MapComponent],
   templateUrl: './procedure.component.html',
-  styleUrl: './procedure.component.scss'
+  styleUrl: './procedure.component.scss',
 })
 export class ProcedureComponent implements OnInit {
   isLoading: boolean = true;
 
+  constructor() {}
+
   ngOnInit(): void {
     setTimeout(() => {
-      this.isLoading = false
-    }, 2000)
+      this.isLoading = false;
+    }, 2000);
   }
 }
